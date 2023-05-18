6bdd909ca64f1cabf9914df607a0dc675c20da4d
Sergej Frank <sergejf2011@gmail.com>
2023-05-18 17:53:34 +0200

    added inventory updating

--- index.js
+++ index.js
@@ -197,8 +197,8 @@ async function processPage(){
 
 async function processCategories(){
 
-    const token = process.env.TOKEN;
-    const api_base_url = process.env.API_BASE_URL;
+    const token = process.env.ECWID_API_TOKEN;
+    const api_base_url = process.env.ECWID_API_BASE_URL;
     const url = api_base_url+'categories';//+'?token='+token;
 
     let offset = 100;
@@ -228,8 +228,8 @@ async function processEntity(entity){
 
 async function processEntityPart(entity, offset){
 
-    const token = process.env.TOKEN;
-    const api_base_url = process.env.API_BASE_URL;
+    const token = process.env.ECWID_API_TOKEN;
+    const api_base_url = process.env.ECWID_API_BASE_URL;
     const url = api_base_url+entity;
 
     var response = await axios.get(url, {
@@ -245,8 +245,6 @@ async function processEntityPart(entity, offset){
         await saveEntityPart(entity, offset, data);
         await sleep(1000);
 
-        //await processEntityEntries(entity, data);
-
         await processEntityPart(entity, offset+100);
     }
 }
@@ -257,24 +255,229 @@ async function saveEntityPart(entity, offset, data){
 }
 
 
-async function processEntityEntries(entity, data){
-    
-    const items = data.items;
 
-    for(var i=0; i<items.length; i++){
+//console.dir(jsonfile.readFileSync(file))
 
+var shopifyInstance = axios.create({
+    baseURL: process.env.SHOPIFY_API_URL,
+    timeout: 20000,
+    headers: {
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_TOKEN
+    },
+    validateStatus: function (status) {
+        return status >= 200 && status < 500; // default
+    },
+});
 
+async function getShopifyProduct(id = null){
 
+    if(id){
+        var response = await shopifyInstance.get(`products/${id}.json`);
+        if(response.status != 200){
+            console.error('ERROR: getProduct response.status= ', response.status);
+            return 0;
+        }
+
+        var product = response.data.product;
+        console.log('getShopifyProduct: response.status: ', response.status);
+        console.log('getShopifyProduct: product: ', id, product);
+    }
+    else {
+        var response = await shopifyInstance.get('products.json');
+        var products = response.data.products;
+        console.log('getShopifyProduct: response.status: ', response.status);
+        console.log('getShopifyProduct: first product in products request: ', products.length, products[0]);
     }
 }
 
-//console.dir(jsonfile.readFileSync(file))
+
+async function setShopifyProductVariant(id, quantity){
+
+    //arguments: product_id/ variant_id, variantProps
+
+        var response = await shopifyInstance.get(`products/${id}.json`);
+        if(response.status != 200){
+            console.error('ERROR: setShopifyProductVariant: productFetching: response.status= ', response.status);
+            return 0;
+        }
+
+        //console.log('getShopifyProduct: response.status: ', response.status);
+        //console.log('getShopifyProduct: product: ', id, product);
+
+        var product = response.data.product;
+        var variant = product.variants[0];
+        console.log('variant.id: ', variant.id);
+
+        var updateResponse = await shopifyInstance.put(`/variants/${variant.id}.json`, {
+            variant: {
+                id: variant.id,
+                price: '10.10',
+                //inventory_quantity: quantity
+            }
+        });
+
+        if(updateResponse.status != 200){
+            console.error('ERROR: setShopifyProductVariant variantUpdate updateResponse.status= ', updateResponse.status);
+            return 0;
+        }
+
+        variant = updateResponse.data.variant;
+        
+        console.log('setShopifyProductVariant variant: ', variant);
+}
+
+
+async function getFirstLocationId(){
+    var localtionsResponse = await shopifyInstance.get(`locations.json`);
+    if(localtionsResponse.status != 200){
+        console.error('ERROR: setShopifyProductInventory: productFetching: status= ', localtionsResponse.status);
+        return null;
+    }
+
+    var locations = localtionsResponse.data.locations;
+    //console.log('locations: ', locations);
+
+    var first_location_id = locations[0].id;
+    return first_location_id;
+}
+
+async function setInventoryManagementValue(variant_id, inventory_management){
+    //return 1;
+    var updateVariantResponse = await shopifyInstance.put(`/variants/${variant_id}.json`, {
+        variant: {
+            id: variant_id,
+            inventory_management: inventory_management,
+            //inventory_policy: 'deny', //default: deny. 'continue', 'deny' -> when out of stock, continue selling or deny?
+            //price: '10.10',
+            //inventory_quantity: quantity
+        }
+    });
+
+    if(updateVariantResponse.status != 200){
+        console.error('ERROR: setShopifyProductVariant variantUpdate updateVariantResponse.status= ', updateVariantResponse.status);
+        return 0;
+    }
+
+    var variant = updateVariantResponse.data.variant;
+    
+    console.log('setShopifyProductVariant inventory_management to for variant_id: ', variant.inventory_management, variant.id);
+    return 1;
+}
+
+
+async function setShopifyProductInventory(location_id, product_id, quantity){
+
+    var response = await shopifyInstance.get(`products/${product_id}.json`);
+    if(response.status != 200){
+        console.error('ERROR: setShopifyProductInventory: productFetching: product_id, status= ', product_id, response.status);
+        return 0;
+    }
+
+    //console.log('getShopifyProduct: response.status: ', response.status);
+    var product = response.data.product;
+    var variant = product.variants[0];
+    var inventory_item_id = variant.inventory_item_id;
+    // console.log('getShopifyProduct: product: ', id, product);
+    console.log('variant.id, inventory_item_id: ', variant.id, inventory_item_id);
+    //return 1;
+
+    var inventory_management = null;
+    /**/
+    if(quantity){
+        inventory_management = 'shopify';
+        var valueUpdated = await setInventoryManagementValue(variant.id, inventory_management);
+        if(!valueUpdated){
+            console.error('ERROR: setShopifyProductInventory: setInventoryManagementValue: product_id, status= ', product_id, response.status);
+            return 0;
+        }
+
+        var inventoryLevelUpdateResponse = await shopifyInstance.post(`/inventory_levels/set.json`, {
+            location_id: location_id,
+            inventory_item_id: inventory_item_id,
+            available: quantity,
+        });
+    
+        if(inventoryLevelUpdateResponse.status != 200){
+            console.error('ERROR: setShopifyProductVariant inventoryLevelUpdateResponse status= ', inventoryLevelUpdateResponse.status, inventoryLevelUpdateResponse.data);
+            return 0;
+        }
+    
+        var inventory_level = inventoryLevelUpdateResponse.data.inventory_level;
+        
+        var newQuantity = inventory_level.available;
+        console.log('setShopifyProductInventory: updated variant quantity to ', newQuantity);
+        //console.log('setShopifyProductVariant inventory_level: ', inventory_level);
+    }
+    else {
+        //set variant inventory_management = null
+
+        var result = await setInventoryManagementValue(variant.id, inventory_management);
+    }
+    
+    /*
+    var inventoryLevelResponse = await shopifyInstance.get(`/inventory_levels.json`, {
+        params: {
+            inventory_item_ids: inventory_item_id
+        }
+    });
+    if(inventoryLevelResponse.status != 200){
+        console.error('ERROR: setShopifyProductInventory: inventoryLevelResponse: status= ', inventoryLevelResponse.status);
+        return 0;
+    }
+
+    var inventory_levels = inventoryLevelResponse.data.inventory_levels;
+    console.log('inventory_levels: ', inventory_levels);
+    return 1;    
+    */
+
+
+    /*
+    var inventoryResponse = await shopifyInstance.get(`/inventory_items/${inventory_item_id}.json`);
+    if(inventoryResponse.status != 200){
+        console.error('ERROR: setShopifyProductInventory: inventoryResponse: status= ', inventoryResponse.status);
+        return 0;
+    }
+
+    var inventory_item = inventoryResponse.data.inventory_item;
+    console.log('inventory_item: ', inventory_item);
+    return 1;    
+    */
+
+    /*
+    var updateResponse = await shopifyInstance.put(`/inventory_item/${inventory_item_id}.json`, {
+        inventory_item: {
+            id: inventory_item_id,
+            tracked: false,
+            //price: '10.10',
+            //inventory_quantity: quantity
+        }
+    });
+
+    if(updateResponse.status != 200){
+        console.error('ERROR: setShopifyProductInventory updateResponse status= ', updateResponse.status, updateResponse.data);
+        return 0;
+    }
+
+    var inventory_item = updateResponse.data.inventory_item;
+    
+    console.log('setShopifyProductInventory updateResponse inventory_item: ', inventory_item);
+    */
+}
 
 
 (async () => {
     
-    await processEntity('categories');
-    await processEntity('products');
+    var product_id = 8388265476415;
+    var quantity = 2;
+
+    var location_id = await getFirstLocationId();
+    //return 0;
+    await setShopifyProductInventory(location_id, product_id, quantity);
+    //await getShopifyProduct(id);
+    
+
+    //await processEntity('categories');
+    //await processEntity('products');
     //await processCategories();
     //await processPage();
 
